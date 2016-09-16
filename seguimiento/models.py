ee8c92e0e58c25e75d479ffe2faadd0cf820a6a7
Ronald Suez <rsuez93@gmail.com>
2016-09-15 20:01:58 -0400

    ordenando

--- seguimiento/models.py
+++ seguimiento/models.py
@@ -30,67 +30,6 @@ ACTIVIDADES = (
 )
 
 
-
-class CultivoMuestra(models.Model):
-	codigo = models.CharField(max_length=255)
-	ubicacion = models.CharField(max_length=255)
-	cultivo = models.ForeignKey(Cultivo, related_name='muestras')
-	fecha_registro = models.DateField(auto_now_add=True)
-
-	def __str__(self):
-			return '%s - %s ' % (self.codigo, self.cultivo.codigo)
-
-	def __unicode__(self):
-			return '%s - %s ' % (self.codigo, self.cultivo.codigo)
-
-
-
-class CrecimientoCultivo(models.Model):
-	MEDIDAS = (
-        (1, 'cm'),
-        (2, 'mm'),
-	)
-
-	muestra_cultivo = models.ForeignKey(CultivoMuestra, related_name='crecimiento_muestra')
-	medida = models.FloatField(default=0.0)
-	unidad = models.IntegerField(default=1, choices=MEDIDAS)
-	fecha_registro = models.DateField(auto_now=True)
-	observaciones = models.TextField(max_length=255)
-	qrcode = models.ImageField(upload_to='qrcode', blank=True, null=True)
-
-	def generate_qrcode(self):
-
-		qr = qrcode.QRCode(
-			version=1,
-			error_correction=qrcode.constants.ERROR_CORRECT_L,
-			box_size=6,
-			border=0,
-		)
-		qr.add_data(self.pk)
-		qr.make(fit=True)
-
-		img = qr.make_image()
-
-		buffer = StringIO.StringIO()
-		img.save(buffer)
-		filename = 'cultivo-muestra-%s.png' % (self.id)
-		filebuffer = InMemoryUploadedFile(
-			buffer, None, filename, 'image/png', buffer.len, None)
-		self.qrcode.save(filename, filebuffer)
-
-
-class Plaga(models.Model):
-	nombre = models.CharField(max_length=255)
-	descripcion = models.TextField(max_length=255)
-	imagen = models.CharField(max_length=255)
-
-
-class PlagasCultivo(models.Model):
-	fecha_aparacion = models.DateField()
-	plaga = models.ForeignKey(Plaga)
-	imagen = models.CharField(max_length=255)
-	cultivo = models.ForeignKey(Cultivo)
-
 class Insumo(models.Model):
 	nombre = models.CharField(max_length=255)
 	codigo = models.CharField(max_length=255, blank=True)
@@ -102,6 +41,20 @@ class Insumo(models.Model):
 
 	def __unicode__(self):
 			return '%s' % self.nombre
+
+class Plaga(models.Model):
+	nombre = models.CharField(max_length=255)
+	descripcion = models.TextField(max_length=255)
+	imagen = models.CharField(max_length=255)
+
+
+
+class PlagasCultivo(models.Model):
+	fecha_aparacion = models.DateField()
+	plaga = models.ForeignKey(Plaga)
+	imagen = models.CharField(max_length=255)
+	cultivo = models.ForeignKey(Cultivo)
+
 
 
 class CosechaCultivo(models.Model):
@@ -159,3 +112,55 @@ class ActividadesCultivo(models.Model):
 
 	def __unicode__(self):
 			return '%s ' % self.cultivo.codigo
+
+
+
+
+
+class CultivoMuestra(models.Model):
+	codigo = models.CharField(max_length=255)
+	ubicacion = models.CharField(max_length=255)
+	cultivo = models.ForeignKey(Cultivo, related_name='muestras')
+	fecha_registro = models.DateField(auto_now_add=True)
+
+	def __str__(self):
+			return '%s - %s ' % (self.codigo, self.cultivo.codigo)
+
+	def __unicode__(self):
+			return '%s - %s ' % (self.codigo, self.cultivo.codigo)
+
+
+
+class CrecimientoCultivo(models.Model):
+	MEDIDAS = (
+        (1, 'cm'),
+        (2, 'mm'),
+	)
+
+	muestra_cultivo = models.ForeignKey(CultivoMuestra, related_name='crecimiento_muestra')
+	medida = models.FloatField(default=0.0)
+	unidad = models.IntegerField(default=1, choices=MEDIDAS)
+	fecha_registro = models.DateField(auto_now=True)
+	observaciones = models.TextField(max_length=255)
+	qrcode = models.ImageField(upload_to='qrcode', blank=True, null=True)
+
+	def generate_qrcode(self):
+
+		qr = qrcode.QRCode(
+			version=1,
+			error_correction=qrcode.constants.ERROR_CORRECT_L,
+			box_size=6,
+			border=0,
+		)
+		qr.add_data(self.pk)
+		qr.make(fit=True)
+
+		img = qr.make_image()
+
+		buffer = StringIO.StringIO()
+		img.save(buffer)
+		filename = 'cultivo-muestra-%s.png' % (self.id)
+		filebuffer = InMemoryUploadedFile(
+			buffer, None, filename, 'image/png', buffer.len, None)
+		self.qrcode.save(filename, filebuffer)
+
